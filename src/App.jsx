6edc16a4edd3755de86d5eb130f975eeb9d5b8d0
daviardev <jerson.silva99@hotmail.com>
2023-08-12 18:39:06 -0500

    Use header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import Header from './components/Header'
+
 export default function App () {
   return (
     <>
+      <main className='min-h-screen flex max-w-[1500px] mx-auto'>
+        <Header />
+      </main>
     </>
   )
 }
