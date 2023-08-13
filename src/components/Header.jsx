1ee18b4de46b11827e18e55fa761ca4012dd64ab
daviardev <jerson.silva99@hotmail.com>
2023-08-12 23:11:38 -0500

    Change theme when click button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import Logo from './Icons/Logo'
 
+import { useTheme } from '../context/ThemeContext'
+
 export default function Header () {
+  const { toggleTheme } = useTheme()
+
   return (
     <>
       <header className='flex justify-center items-center h-[74px] w-full'>
-        <button className='w-[24px] h-[24px]'>
+        <button className='w-[24px] h-[24px]' onClick={toggleTheme}>
           <span>
             <Logo />
           </span>
