380e0dbff0e11fced70458033aeccd8f8b394cb2
daviardev <jerson.silva99@hotmail.com>
2023-08-14 09:25:44 -0500

    Use theme provider and use components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import Header from './components/Header'
+import UserInfo from './components/UserInfo'
+
+import { ThemeProvider } from './context/ThemeContext'
 
 export default function App () {
   return (
     <>
-      <main className='min-h-screen flex max-w-[1500px] mx-auto'>
-        <Header />
-      </main>
+      <ThemeProvider>
+        <main>
+          <Header />
+          <div>
+            <UserInfo />
+          </div>
+        </main>
+      </ThemeProvider>
     </>
   )
 }
