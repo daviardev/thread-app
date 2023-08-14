a21ab72b179084d17002bb60ac993bbb32d4e3c6
daviardev <jerson.silva99@hotmail.com>
2023-08-14 16:58:40 -0500

    Fix position icons

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -12,37 +12,32 @@ export default function Likes () {
             </div>
           </div>
         </div>
-        <div className='min-w-0 flex-grow-0'>
+        <div className='min-w-0 flex-grow'>
           <div className='flex items-center'>
             <span className='pr-1.5 inline-block pl-1.5'>
               <span className='restart-positions name-subtitle' style={{ lineHeight: 'calc(1.4*1em)' }}>
                 ·
               </span>
             </span>
-            <span className='min-w-0'>
+            <div className='min-w-0'>
               <a className='restart-positions name-subtitle' href='https://www.udemy.com/course/python-django-2021-complete-course/'>
-                <span className='text-[15px] w-[160px] block overflow-hidden text-ellipsis whitespace-nowrap'>udemy.com/course/python-django-2021-complete-course</span>
+                <span className='text-[15px] w-[155px] md:w-full block overflow-hidden text-ellipsis whitespace-nowrap'>udemy.com/course/python-django-2021-comlete-course</span>
               </a>
-            </span>
+            </div>
           </div>
         </div>
         <div className='flex items-center ml-4 flex-grow-0'>
-          <div>
-            <button className='restart-positions name-subtitle'>
-              <div className='cursor-pointer restart-positions'>
-                <Instagram />
-              </div>
-            </button>
-          </div>
-          <div className='flex items-center ml-4 flex-grow-0'>
-            <div>
-              <button className='restart-positions name-subtitle'>
-                <div className='cursor-pointer restart-positions'>
-                  <Dots />
-                </div>
-              </button>
+          <button className='restart-positions name-subtitle'>
+            <div className='cursor-pointer restart-positions'>
+              <Instagram />
             </div>
-          </div>
+          </button>
+          <div className='ml-4' />
+          <button className='restart-positions name-subtitle'>
+            <div className='cursor-pointer restart-positions'>
+              <Dots />
+            </div>
+          </button>
         </div>
       </div>
     </>
