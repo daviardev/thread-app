cf8cff539569dcccb61212cd50b8251f1034aa8d
daviardev <jerson.silva99@hotmail.com>
2023-08-13 20:50:43 -0500

    Remove context in main file

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 import { $ } from './utils/dom'
-import { ThemeProvider } from './context/ThemeContext.jsx'
 
 import './index.css'
 
 ReactDOM.createRoot($('#root')).render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
+  <App />
 )
