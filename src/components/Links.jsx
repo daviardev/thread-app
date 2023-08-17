f5bc30d269a372d82470f08c2fe3e3ef18c28a34
daviardev <jerson.silva99@hotmail.com>
2023-08-16 23:18:55 -0500

    Organizate tags and styles, use modal component

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,45 +1,52 @@
+import { useState } from 'react'
+
 import Dots from './Icons/Dots'
+import Modal from './Modal'
 import Instagram from './Icons/Instagram'
 
 export default function Likes () {
+  const [isOpen, setIsOpen] = useState()
+
+  const handleOpen = () => {
+    setIsOpen(prev => !prev)
+  }
+
+  const handleClose = e => {
+    e.target.closest() && setIsOpen()
+  }
   return (
     <>
-      <div className='mt-[18px] min-h-[22px] flex items-center'>
-        <div className='flex flex-shrink-0'>
-          <div className='restart-positions'>
-            <div className='flex items-center'>
-              <a className='restart-positions name-subtitle' style={{ lineHeight: 'calc(1.4*1em)' }}>241 seguidores</a>
-            </div>
-          </div>
-        </div>
-        <div className='min-w-0 flex-grow'>
-          <div className='flex items-center'>
-            <span className='pr-1.5 inline-block pl-1.5'>
-              <span className='restart-positions name-subtitle' style={{ lineHeight: 'calc(1.4*1em)' }}>
-                ·
-              </span>
+      <section className='mt-[18px] min-h-[22px] flex items-center' onClick={handleClose}>
+        <a className='flex items-center flex-shrink-0 restart-positions name-subtitle'>
+          241 seguidores
+        </a>
+        <div className='min-w-0 flex-grow flex items-center'>
+          <span className='pr-1.5 inline-block pl-1.5 restart-positions name-subtitle'>
+            ·
+          </span>
+          <a className='min-w-0 restart-positions name-subtitle' href='https://www.udemy.com/course/python-django-2021-complete-course/'>
+            <span className='w-full block overflow-hidden text-ellipsis whitespace-nowrap'>
+              udemy.com/course/python-django-2021-complete-course/
             </span>
-            <div className='min-w-0'>
-              <a className='restart-positions name-subtitle' href='https://www.udemy.com/course/python-django-2021-complete-course/'>
-                <span className='text-[15px] w-[155px] md:w-full block overflow-hidden text-ellipsis whitespace-nowrap'>udemy.com/course/python-django-2021-comlete-course</span>
-              </a>
-            </div>
-          </div>
+          </a>
         </div>
         <div className='flex items-center ml-4 flex-grow-0'>
-          <button className='restart-positions name-subtitle'>
-            <div className='cursor-pointer restart-positions'>
+          <a>
+            <button className='restart-positions'>
               <Instagram />
-            </div>
-          </button>
+            </button>
+          </a>
           <div className='ml-4' />
-          <button className='restart-positions name-subtitle'>
-            <div className='cursor-pointer restart-positions'>
+          <button className='cursor-pointer' onCLick={handleOpen}>
+            <div className='restart-positions'>
               <Dots />
             </div>
           </button>
+          <Modal isOpen={isOpen}>
+            Copiar enlace
+          </Modal>
         </div>
-      </div>
+      </section>
     </>
   )
 }
