6068d13de86cec0b9cc8376ffc0ad09de091d8cb
daviardev <jerson.silva99@hotmail.com>
2023-08-16 23:16:48 -0500

    Change width, height and add style for the logo

--- src/components/Icons/Logo.jsx
+++ src/components/Icons/Logo.jsx
@@ -1,9 +1,10 @@
 const Logo = props => (
   <svg
     xmlns='http://www.w3.org/2000/svg'
-    width={12}
-    height={12}
+    width={24}
+    height={24}
     fill='none'
+    className='relative block'
     aria-label='Threads'
     viewBox='0 0 192 192'
     {...props}
