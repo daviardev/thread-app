d498bbbeda2d164a741a69710c674c6a0deb2bec
daviardev <jerson.silva99@hotmail.com>
2023-08-16 23:18:13 -0500

    Finish modal component and add funcionality

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,18 @@
-export default function Modal () {
-  const options = ['Copiar enlace', 'Reportar']
+export default function Modal ({ isOpen, children }) {
+  if (!isOpen) {
+    return null
+  }
   return (
     <>
-      <div className='bg-[var(--card-color)] top-0 absolute'>
-        <div className=''>
-          d
-        </div>
+      <div
+        className='border-[var(--border-card)] rounded-2xl z-10 border-[.5px] w-auto bg-[var(--card-color)] cursor-pointer absolute mt-24 -ml-[80px] pl-4 pb-4 pt-4 pr-8'
+        style={{
+          boxShadow: 'var(--shadow-card)'
+        }}
+      >
+        <span className='font-semibold after:content-[""] before:content-[""]'>
+          {children}
+        </span>
       </div>
     </>
   )
