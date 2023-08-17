fd567870182bfc4b372412959faadb1640b1fa97
daviardev <jerson.silva99@hotmail.com>
2023-08-16 23:17:51 -0500

    Organizate tags and styles

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,20 +1,13 @@
 export default function Options () {
   return (
     <>
-      <div className='mb-4'>
-        <div className='grid w-full items-center' style={{ gridTemplateColumns: 'repeat(2,1fr)' }}>
-          <a href='#' className='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd xggy1nq x1a2a7pz x6s0dn4 x78zum5 xdt5ytf x1iyjqo2 xsdox4t xl56j7k x1n2onr6 x2b8uid x1citr7e x1agx3nx x1q0q8m5 xso031l x1hl2dhg x1lku1pv name-subtitle'>
-            <span className='name-subtitle'>
-              Hilos
-            </span>
-            <div class='xos1y1y x1q0q8m5 xso031l bottom-[15px] border-b-[var(--primary-text-color)] right-0 will-change-transform absolute left-0' style={{ transformOrigin: 'left top' }} />
-          </a>
-          <a href='#' className='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd xggy1nq x1a2a7pz x6s0dn4 x78zum5 xdt5ytf x1iyjqo2 xsdox4t xl56j7k x1n2onr6 x2b8uid x1citr7e x1agx3nx x1q0q8m5 xso031l x1hl2dhg x1lku1pv name-subtitle'>
-            <span className='name-subtitle'>
-              Respuestas
-            </span>
-          </a>
-        </div>
+      <div className='grid w-full items-center' style={{ gridTemplateColumns: 'repeat(2,1fr)' }}>
+        <a href='#' className='border-b h-12 text-center name-subtitle'>
+          Hilos
+        </a>
+        <a href='#' className='border-b text-center h-12 name-subtitle'>
+          Respuestas
+        </a>
       </div>
     </>
   )
