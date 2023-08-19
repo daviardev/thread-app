33a51810c277103a9870b05bc40871e176d4519d
daviardev <jerson.silva99@hotmail.com>
2023-08-18 21:42:12 -0500

    Add current active option

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -2,10 +2,10 @@ export default function Options () {
   return (
     <>
       <div className='grid w-full items-center' style={{ gridTemplateColumns: 'repeat(2,1fr)' }}>
-        <a href='#' className='border-b h-12 text-center name-subtitle'>
+        <a href='#' className='h-12 text-center name-subtitle current'>
           Hilos
         </a>
-        <a href='#' className='border-b text-center h-12 name-subtitle'>
+        <a href='#' className='text-center h-12 name-subtitle'>
           Respuestas
         </a>
       </div>
