486a4d182c5063c97c9daca92a15627e82a8a939
daviardev <jerson.silva99@hotmail.com>
2023-08-18 21:45:04 -0500

    Copy text event

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,7 +5,8 @@ export default function Modal ({ isOpen, children }) {
   return (
     <>
       <div
-        className='border-[var(--border-card)] rounded-2xl z-10 border-[.5px] w-auto bg-[var(--card-color)] cursor-pointer absolute mt-24 -ml-[80px] pl-4 pb-4 pt-4 pr-8'
+        onClick={() => navigator.clipboard.writeText('username')}
+        className='border-[var(--border-card)] rounded-2xl z-10 border-[.5px] w-auto bg-[var(--card-color)] cursor-pointer absolute mt-24 -ml-[80px] pl-4 pb-4 pt-4 pr-8 hover:bg-[var(--hover)]'
         style={{
           boxShadow: 'var(--shadow-card)'
         }}
